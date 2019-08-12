c8eb48b381ae3ac3aa25b1fe522e3f9c6de63f7c
Martin Ruble <martinruble@martins-air.lan>
2019-08-12 08:59:22 -0600

    start with interactive cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	clearScreen()
 	fmt.Println("...running nature.")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Enter Text: ")
+		// Scans a line from Stdin(Console)
+		scanner.Scan()
+		// Holds the string that scanned
+		text := scanner.Text()
+		if len(text) != 0 {
+			fmt.Println(text)
+		}
+		switch text {
+		case "x":
+			os.Exit(0)
+		case "l":
+			createCard(GetLion())
+		default:
+			break
+		}
+
+	}
+
 	l := GetLion()
 	d := GetAfricanDog()
 	tm := GetThomGaz()
